9e97aca75d5bda7d6f79c2511813196691130895
Ary Akhyar <aryart1604@gmail.com>
2025-06-26 20:54:50 +0700

    baru new

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Pas diklik Menu
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik luar sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
